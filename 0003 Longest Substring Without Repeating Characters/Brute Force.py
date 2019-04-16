c37514e1f3d532cf8c036498f5355233b8f0d79a
lplusk <49985640@qq.com>
2019-04-17 00:10:19 +0800

    0003 Longest Substring Without Repeating Characters

---
+++ 0003 Longest Substring Without Repeating Characters/Brute Force.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        def all_unique(s, start, end):
+            cs = set()
+            for i in range(start, end):
+                ch = s[i]
+                if ch in cs:
+                    return False
+                cs.add(ch)
+            return True
+        
+        n = len(s)
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                if all_unique(s, i, j):
+                    ans = max(ans, j - i)
+        
+        return ans
